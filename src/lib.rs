646dfd97f644d53ab00491837fa1488b1c9d6b8e
saresend <saresend@usc.edu>
2018-03-20 20:12:25 -0700

    adding reqwest

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
+extern crate reqwest;
+
+use std::io::Result;
+
+pub struct Reddit {}
+
+impl Reddit {
+    pub fn new(client_id: &str) -> Result<Reddit> {
+        let client = reqwest::Client::new();
+        let url = client
+            .get("https://www.reddit.com/api/v1/authorize")
+            .query(&[("response_type", "code"), ("client_id", client_id)])
+            .send()
+            .unwrap();
+        unimplemented!();
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
