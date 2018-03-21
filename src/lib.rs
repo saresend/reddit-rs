94fd34454940ac3d23605809a9f168f62fe423e1
saresend <saresend@usc.edu>
2018-03-20 20:20:22 -0700

    adding auth

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,26 @@
 extern crate reqwest;
 
 use std::io::Result;
+use std::collections::HashMap;
 
 pub struct Reddit {}
 
 impl Reddit {
-    pub fn new(client_id: &str) -> Result<Reddit> {
+    pub fn new(
+        client_id: &str,
+        client_secret: &str,
+        username: &str,
+        password: &str,
+    ) -> Result<Reddit> {
+        let mut map = HashMap::new();
+        map.insert("grant_type", "password");
+        map.insert("username", username);
+        map.insert("password", password);
         let client = reqwest::Client::new();
         let url = client
-            .get("https://www.reddit.com/api/v1/authorize")
-            .query(&[("response_type", "code"), ("client_id", client_id)])
+            .post("https://www.reddit.com/api/v1/access_token")
+            .basic_auth(client_id, Some(password))
+            .json(&map)
             .send()
             .unwrap();
         unimplemented!();
